1bbf324660832aa0e9db75ae447c75d1138c654e
John Parent <john.parent@kitware.com>
2023-01-12 11:06:18 -0500

    Win builder

---
+++ .github/workflows/win_builder_help.ps1
@@ -0,0 +1,8 @@
+spack compiler find
+spack external find python
+spack external find cmake
+spack external find ninja
+spack external find msmpi
+spack external find win-wdk
+spack external find win-sdk
+spack external find wgl
