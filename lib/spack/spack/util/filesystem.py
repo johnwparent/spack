42962a7db1bc26aa52677548edba82cd1fb27e46
John Parent <john.parent@kitware.com>
2025-07-22 13:24:15 -0400

    relocate win rpath: ensure wrapper is aware of install root
    To determine when relocation is unneccesary post install (i.e. for another spack dep) we need to know what defines a "spack installed dep".
    We do this by informing the wrapper of the spack install prefix root so it can determine if any of the "rpath"'d dependencies have the same prefix.

--- lib/spack/spack/util/filesystem.py
+++ lib/spack/spack/util/filesystem.py
@@ -11,6 +11,7 @@ import glob
 import os
 import re
 
+import spack.store
 from spack.llnl.util.filesystem import edit_in_place_through_temporary_file
 from spack.util.executable import Executable
 
@@ -89,6 +90,7 @@ def relocate_win_rpath(package):
             if not (entry.is_symlink() or entry.is_junction()) and entry.is_dir():
                 dirs_to_relocate.append(entry.path)
     ev.set_path("SPACK_RELOCATE_PATH", dirs_to_relocate)
+    ev.set("SPACK_INSTALL_PREFIX", spack.store.STORE.root)
     lib_map = {}
     for lib in glob.glob(os.path.join(package.spec.prefix, "**\\*.lib"), recursive=True):
         if verify_import_lib(lib, package=package):
